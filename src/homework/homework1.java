f93ff8c62be9f14fcdc6499eb22f21d3ddb837dc
Harut Ghandilyan <h.ghandilyan21@gmail.com>
2023-08-29 15:37:11 +0400

    test commit

--- src/homework/homework1.java
+++
@@ -1,9 +0,0 @@
-package homework;
-
-public class homework1 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 6; i++) {
-            System.out.println("* * * * * * ");
-        }
-    }
-}
