3e381f28a3e0d84be3d2467d8337c71d11918ea0
Harut Ghandilyan <h.ghandilyan21@gmail.com>
2023-08-30 03:32:56 +0400

    test commit

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -38,18 +38,45 @@ public class FigurePainter {
         System.out.println();
         //4rd
         for (int i = 0; i < 5; i++) {
-            for (int j = 1; j <=i ; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
-                
+
             }
-            for (int k = 5; k >i ; k--) {
+            for (int k = 5; k > i; k--) {
                 System.out.print("*");
-                
+
             }
             System.out.println();
-            
+
         }
         System.out.println();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 3; j >= i; j--) {
+                System.out.print(" ");
+
+
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+
+
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
+
+
+            }
+            System.out.println();
+
+        }
     }
 }
 
