f362faa8d53f76bc4494b9e925164412582a3ea4
Harut Ghandilyan <h.ghandilyan21@gmail.com>
2023-08-29 15:45:37 +0400

    test commit

---
+++ src/classwork/chapter3/NumbersArrayExample.java
@@ -0,0 +1,9 @@
+package classwork.chapter3;
+
+public class NumbersArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {3, 5, 1, 77, 23, 88, 12, 232, 112};
+        int LastIndex = numbers.length - 1;
+        System.out.println(numbers[LastIndex]);
+    }
+}
---
+++ src/homework/FigurePainter.java
@@ -0,0 +1,61 @@
+package homework;
+
+//1st
+public class FigurePainter {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+        //2nd
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //3rd
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //4rd
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(" ");
+                
+            }
+            for (int k = 5; k >i ; k--) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+            
+        }
+        System.out.println();
+    }
+}
+
+
+
+
+
+
+
